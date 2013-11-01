51da4a375538fabfaad2d28a7a13e1f74031895d
Muflone <webreg@vbsimple.net>
2013-11-01 19:49:25 +0100

    Implemented pause in the DaemonThread class

--- bluewho/daemon_thread.py
+++ bluewho/daemon_thread.py
@@ -21,14 +21,26 @@
 from gi.repository import Gdk
 from gi.repository import GObject
 from threading import Thread
+from threading import Event
 
 class DaemonThread(Thread):
-  def __init__(self, target):
+  def __init__(self, target, name):
     GObject.threads_init()
     Gdk.threads_init()
     super(self.__class__, self).__init__(target=target)
+    self.name = name
     self.daemon = True
     self.cancelled = False
+    self.paused = False
+    self.event = Event()
 
   def cancel(self):
     self.cancelled = True
+
+  def pause(self):
+    self.paused = True
+
+  def unpause(self):
+    self.paused = False
+    print 'unpausing'
+    self.event.set()
